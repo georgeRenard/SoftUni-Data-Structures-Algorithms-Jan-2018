164c7b0f053f6b60d68de75449288fe2da0eda95
georgeRenard <goshoangelov97@gmail.com>
2018-01-09 00:11:55 +0200

    Add files via upload

---
+++ DataStructures/01.Linear-Data-Structures-Lab/Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+public class ArrayList<T> : IEnumerable<T>
+{
+
+    private T[] arr;
+
+    public int Count { get; set; }
+    public int Capacity { get; set; }
+
+    public ArrayList(int capacity = 2)
+    {
+        this.arr = new T[capacity];
+        this.Capacity = capacity;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return this.arr[index];
+        }
+
+        set
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this.arr[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.Count + 1 > this.Capacity)
+        {
+            this.Grow();
+        }
+        this.arr[this.Count] = item;
+        this.Count++;
+    }
+
+    public T RemoveAt(int index)
+    {
+        T item = this[index];
+        this[index] = default(T);
+        this.ShiftLeft(index);
+        if (this.Count - 1 < this.Capacity / 3)
+        {
+            this.Shrink();
+        }
+        this.Count--;
+        return item;
+    }
+
+    private void Grow()
+    {
+        T[] newArr = new T[this.Capacity * 2];
+        this.Capacity *= 2;
+        Array.Copy(this.arr, newArr, this.Count);
+        this.arr = newArr;
+    }
+
+    private void Shrink()
+    {
+        T[] newArr = new T[this.Capacity / 2];
+        this.Capacity /= 2;
+        Array.Copy(this.arr, newArr, this.Count);
+        this.arr = newArr;
+    }
+
+    private void ShiftLeft(int index)
+    {
+        for (int i = index; i < this.Count - 1; i++)
+        {
+            this.arr[i] = this.arr[i + 1];
+        }
+
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var item in this.arr)
+        {
+            yield return item;
+        }
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ DataStructures/01.Linear-Data-Structures-Lab/Linear-Data-Structures/Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        ArrayList<string> list = new ArrayList<string>();
+        list.Add("5");
+        list.Add("6");
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
