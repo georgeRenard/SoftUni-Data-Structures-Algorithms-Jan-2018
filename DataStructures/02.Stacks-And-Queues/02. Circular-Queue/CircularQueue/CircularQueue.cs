f470068baed9ed630e1ab77600c3651e61c1df0b
georgeRenard <goshoangelov97@gmail.com>
2018-01-15 10:25:33 +0200

    Add files via upload

---
+++ DataStructures/02.Stacks-And-Queues/02. Circular-Queue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircularQueue<T> : IEnumerable<T>
+{
+
+    private T[] arr;
+    private int head;
+    private int tail;
+
+    private const int DefaultCapacity = 4;
+    public int Count { get; private set; }
+    public int Capacity { get; private set; }
+
+    public CircularQueue(int capacity = DefaultCapacity)
+    {
+        this.Capacity = capacity;
+        this.head = 0;
+        this.tail = 0;
+        this.Count = 0;
+        this.arr = new T[this.Capacity];
+    }
+
+    public void Enqueue(T element)
+    {
+        if(this.Count >= this.Capacity)
+        {
+            this.Resize();
+        }
+
+        this.arr[tail] = element;
+        this.tail = (this.tail + 1) % this.Capacity;
+        this.Count++;
+    }
+
+    private void Resize()
+    {
+        T[] newArray = new T[this.Capacity * 2];
+        this.CopyAllElements(newArray);
+        this.Capacity *= 2;
+        this.arr = newArray;
+    }
+
+    private void CopyAllElements(T[] newArray)
+    {
+        for (int i = 0; i < this.Count ; i++)
+        {
+            int index = (i + this.head) % this.Capacity;
+            newArray[i] = this.arr[index];
+        }
+        this.head = 0;
+        this.tail = this.Count;
+    }
+
+    // Should throw InvalidOperationException if the queue is empty
+    public T Dequeue()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T element = this.arr[this.head];
+        this.head = (this.head + 1) % this.Capacity;
+        this.Count--;
+        
+        return element;
+    }
+
+    public T[] ToArray()
+    {
+        T[] newArray = new T[this.Count];
+        this.CopyAllElements(newArray);
+        return newArray;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
+
+
+public class Example
+{
+    public static void Main()
+    {
+
+        CircularQueue<int> queue = new CircularQueue<int>();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Dequeue();
+
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        int first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-7);
+        queue.Enqueue(-8);
+        queue.Enqueue(-9);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-10);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+    }
+}
---
+++ DataStructures/02.Stacks-And-Queues/02. Linked-List/LinkedList/LinkedList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+
+    private Node head;
+    private Node tail;
+    
+    public int Count { get; private set; }
+
+    public LinkedList()
+    {
+        this.head = null;
+        this.tail = null;
+        this.Count = 0;
+    }
+
+    public void AddFirst(T item)
+    {
+        Node newNode = new Node(item);
+        if (this.Count == 0)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            newNode.Next = this.head;
+            this.head = newNode;
+        }
+        this.Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        Node newNode = new Node(item);
+        if (this.Count == 0)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            this.tail.Next = newNode;
+            this.tail = newNode;
+        }
+        this.Count++;
+    }
+
+    public T RemoveFirst()
+    {
+
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T element = this.head.Value;
+        if (this.Count == 1)
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else
+        {
+            this.head = this.head.Next;
+        }
+        this.Count--;
+        return element;
+    }
+
+    public T RemoveLast()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T element = this.tail.Value;
+        if (this.Count == 1)
+        {
+            this.head = null;
+            this.tail = null;
+        }
+        else
+        {
+            //Parent
+            Node parent = this.head;
+            while (parent.Next != this.tail)
+            {
+                parent = parent.Next;
+            }
+            parent.Next = null;
+            this.tail = parent;
+        }
+        this.Count--;
+        return element;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node start = this.head;
+        while (start != null)
+        {
+            yield return start.Value;
+            start = start.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class Node
+    { 
+        public T Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+            
+    }
+
+}
